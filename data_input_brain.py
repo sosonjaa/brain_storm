ae0a769b6e1c47d7fde5a97314daad53764e2f62
XiaoMaomify <117036228+XiaoMaomify@users.noreply.github.com>
2023-01-11 12:08:49 +0100

    Create data_input_brain.py
    <3

---
+++ data_input_brain.py
@@ -0,0 +1,25 @@
+file_data = open(r"chartIn.txt")
+file_data_list = []
+
+
+for line in file_data.read().splitlines():  # splits the data into lines
+    file_data_list.append(line.split())     # splits the lines into items
+
+
+def extract_each(number):   # if the question comes up: is there a window seat available?
+    return [item[number] for item in file_data_list]    # returns the value of each row for the given index (aisle)
+
+
+window_seats = extract_each(1) + extract_each(6)  # [row A + row F]
+middle_seats = extract_each(2) + extract_each(5)  # [row B + row E]
+aisle_seats = extract_each(3) + extract_each(4)  # [row C + row D]
+
+
+def reserved_seats_window():
+    if "X" in window_seats:
+        index_window = window_seats.index("X")
+        return print(index_window)
+    else:
+        print("All seats are still available.\n")
+
+file_data.close()
