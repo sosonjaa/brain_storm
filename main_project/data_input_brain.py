41d12f2b3a8c52f4a7d6bfb842b40ee570fd2348
XiaoMaomify <117036228+XiaoMaomify@users.noreply.github.com>
2023-01-11 13:28:53 +0100

    git_update
    added comments

--- main_project/data_input_brain.py
+++ main_project/data_input_brain.py
@@ -1,9 +1,3 @@
-#from pathlib import Path
-
-#p = Path(__file__).with_name('file.txt')
-#with p.open('r') as f:
-#    print(f.read())
-
 file_data_list = []
 
 def open_file(filename):
@@ -18,9 +12,9 @@ def extract_each(number):   # if the question comes up: is there a window seat a
     return [item[number] for item in file_data_list]    # returns the value of each row for the given index (aisle)
 
 
-window_seats = extract_each(1) + extract_each(6)  # [row A + row F]
-middle_seats = extract_each(2) + extract_each(5)  # [row B + row E]
-aisle_seats = extract_each(3) + extract_each(4)  # [row C + row D]
+#window_seats = extract_each(1) + extract_each(6)  # [row A + row F]
+#middle_seats = extract_each(2) + extract_each(5)  # [row B + row E]
+#aisle_seats = extract_each(3) + extract_each(4)  # [row C + row D]
 
 
 def reserved_seats_window():
@@ -31,7 +25,7 @@ def reserved_seats_window():
         print("All seats are still available.\n")
 
 
-file_data.close()
+#file_data.close()
 
 
 # soll alle Dateien einlesen können (chartIn 1-4)
